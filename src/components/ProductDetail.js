c6a2d765c7e1dbc32e0f6d8acfc7f8a12db4a320
vishal <vishalyadav402@gmail.com>
2022-08-02 17:56:57 -0700

    check_product_page

---
+++ src/components/ProductDetail.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Product from './Product';
+import { Button } from 'react-bootstrap';
+
+const ProductDetail = () => {
+  return (
+    <div className='container'>
+        <Product />
+    </div>
+  )
+}
+
+export default ProductDetail
